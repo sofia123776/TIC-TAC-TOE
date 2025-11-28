c5498b57d864723dde898c63ced169b745e5b120
sofia123776 <sofiahafsa00@gmail.com>
2025-11-28 03:45:48 +0300

    first commit

---
+++ .history/src/App_20251127185317.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function Square() {
+  return
+  <div> <button className='square'>X</button>;
+  <div/>
+}
---
+++ .history/src/App_20251128033950.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function Square() {
+  return (
+    <>
+  <div>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  </div>
+  <div>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  </div>
+  <div>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  </div>
+  
+  </>
+  )
+}
+
